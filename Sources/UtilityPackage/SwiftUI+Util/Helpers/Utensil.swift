6c93043b071cac8fdebc2c079bfed107f5093a5c
Shivam Sharma <shivam.sharma@epiphanylabs.in>
2021-03-22 18:58:07 +0530

    Made Classes public.

--- Sources/UtilityPackage/SwiftUI+Util/Helpers/Utensil.swift
+++ Sources/UtilityPackage/SwiftUI+Util/Helpers/Utensil.swift
@@ -8,7 +8,7 @@ import SwiftUI
 /// - Parameters : array of generic type.
 /// Passing input  as an array of models
 
-struct EnumeratedForEach<T, ContentView: View>: View {
+public struct EnumeratedForEach<T, ContentView: View>: View {
     let data: [T]
     let content: (Int, T) -> ContentView
     
@@ -17,14 +17,14 @@ struct EnumeratedForEach<T, ContentView: View>: View {
         self.content = content
     }
     
-    var body: some View {
+    public var body: some View {
         ForEach(Array(self.data.enumerated()), id: \.offset) { idx, item in
             self.content(idx, item)
         }
     }
 }
 
-extension View {
+public extension View {
     @ViewBuilder func isHidden(_ shouldHide: Bool) -> some View {
         switch shouldHide {
         case true: self.hidden()
